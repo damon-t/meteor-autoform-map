e37df5e2be75f41b3b713566b4fce8434bb85612
Damon Tajeddini <48483164+damon-t@users.noreply.github.com>
2019-06-02 13:01:31 +0200

    Update package.js
    Update Package.describe

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'yogiben:autoform-map',
+  name: 'odem:autoform-map',
   summary: 'Edit location coordinates with autoForm',
-  version: '0.2.0',
-  git: 'https://github.com/yogiben/meteor-autoform-map'
+  version: '0.3.0',
+  git: 'https://github.com/damon-t/meteor-autoform-map'
 });
 
 Package.onUse(function(api) {
